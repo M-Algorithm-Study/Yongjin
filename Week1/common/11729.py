33548fe2aad51570f8c6883ca8d6b9e85993aeb3
Kurman11 <skedyd45@naver.com>
2023-06-27 23:04:48 +0900

    [Week1]23.06.27

---
+++ Week1/common/11729.py
@@ -0,0 +1,18 @@
+# 하노이 탑 이동 순서
+
+import sys
+sys.stdin = open('input.txt','r')
+
+def hanoi(num, one, two, three):
+    if num == 1:
+        print(one, two)
+    else:
+        hanoi(num-1, one, three, two)
+        print(one, two)
+        hanoi(num-1, three, two, one)
+
+
+
+num = int(input())
+print((2 ** num) -1)
+hanoi(num, 1,3,2)
