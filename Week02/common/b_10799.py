3c19009f94b0034a67a7656bfe3dcefa9963d0ee
Kurman11 <skedyd45@naver.com>
2023-07-11 10:21:35 +0900

    [Update02]23.07.11

--- Week02/common/b_10799.py
+++ Week02/common/b_10799.py
@@ -8,13 +8,13 @@ count = 0
 open_count = 0  
 
 for i in range(len(T)):
-    if T[i] == '(':
-        open_count += 1
-    else:
-        open_count -= 1
-        if T[i-1] == '(':  
-            count += open_count
+    if T[i] == '(': # T가 ( 일면
+        open_count += 1 # 막대기 생성
+    else: # ) 일때
+        open_count -= 1  # 막대기 여기까지
+        if T[i-1] == '(':  # () 를 완성하고 T의 이전 값이 ( 이면 
+            count += open_count # 레이저로 막대기를 잘랐을때 막대기의 갯수를 더한다
         else:
-            count += 1
+            count += 1 # 만약 이전 값이 ) 이라면 하나의 막대기의 끝 이므로 이후 막대기 개수인 1을 더해 준다.
 
 print(count)
--- Week02/common/p_178870.py
+++ Week02/common/p_178870.py
@@ -1,33 +1,33 @@
 # 연속된 부분 수열의 합
 # 시간초과 발생한 코드....
 
-sequence = [2, 2, 2, 2, 2]
-k = 6
+sequence = [1, 1, 1, 2, 3, 4, 5]
+k = 5
 def solution(sequence, k):
     answer = []
-    b = []
-    result = []
-    cnt = [100]
-    cnt_1 = [1]
-    for i in range(len(sequence)):
-        for x in range(i, len(sequence)):
-            idxs = [i,x]
-            sublist_sum = sum(sequence[i:x+1])
-            if sublist_sum == k:
-                answer.append(idxs)
-                break
-            elif sublist_sum > k:
-                break
-    
-    for start, end in answer:
-        result = end - start
-        if cnt[0] > result:
-            cnt.pop()
-            cnt.append(result)
-            cnt_1.pop()
-            cnt_1.append([start,end])
+    result = 0
+    start = 0
+    sublist_sum = 0 # 누적합
+    min_length = float('inf') #양의 무한대
+    length = 0 # 인덱스 차
+    for end in range(len(sequence)):
+        sublist_sum += sequence[end]
+        
+        while sublist_sum > k: # 누적합이 k를 초과하면
+            sublist_sum -= sequence[start] # 누적합에서 빼주기
+            start += 1 # start값을 증가시켜 누적합 차를 통해 sublist_sum 조건을 맞춘다
+        
+        if sublist_sum == k: # start 부터 end까지 합과 k가 같아지면
+            answer.append((start, end)) # start, end (인덱스 값) append
 
-    for i in cnt_1:
-        return i
+    if answer:
+        for start, end in answer:
+            length = end - start # 인덱스를 빼준다
+            if length < min_length: # 인덱스 값이 min_length 보다 작다면
+                min_length = length # min_length에 length을 넣어주고
+                result = [start, end] # 해당 인덱스 값을 result에 넣어준다
+
+        return result
+
 
 solution(sequence, k)
