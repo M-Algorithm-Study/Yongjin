6b3fcaa139a231f02bb1325830e619ac4529dfb1
Kurman11 <skedyd45@naver.com>
2023-10-21 07:48:21 +0900

    [Week16]23.10.21

--- Week16/common/b_2565.py
+++ Week16/common/b_2565.py
@@ -2,3 +2,17 @@
 
 import sys
 sys.stdin = open('input.txt','r')
+
+T = int(input())
+
+num = [list(map(int,input().split())) for _ in range(T)]
+num.sort()
+
+dp = [1] * T
+
+for i in range(1,T):
+    for j in range(0, i):
+        if num[j][1] < num[i][1]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(T-max(dp))
