b82dd8a63d72db6d7e4f924b2c64f0b3881c998d
Kurman11 <skedyd45@naver.com>
2023-10-23 17:20:49 +0900

    [Week16]23.10.23

---
+++ Week16/individual/b_10773.py
@@ -0,0 +1,12 @@
+# 제로
+
+T = int(input())
+a = []
+for t in range(1, T+1):
+    num = list(map(int,input().split()))
+    for i in num:
+        if i == 0:
+            a.pop()
+        else:
+            a.append(i)
+print(sum(a))
