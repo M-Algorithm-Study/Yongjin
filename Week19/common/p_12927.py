bf4c2b2352911d7aea2f2c05072a6778eddd8b2c
Kurman11 <skedyd45@naver.com>
2023-11-13 17:54:55 +0900

    [Week19]23.11.13

--- Week19/common/p_12927.py
+++ Week19/common/p_12927.py
@@ -1,2 +1,24 @@
 # 야근 지수
 # https://school.programmers.co.kr/learn/courses/30/lessons/12927
+
+import heapq
+
+def solution(n, works):
+    answer = 0
+    lst = []
+    N = sum(works) - n
+    if N <= 0:
+        return 0
+
+    for i in works:
+        heapq.heappush(lst,-i)
+        
+    for _ in range(n):
+        i = heapq.heappop(lst)
+        i = i + 1
+        heapq.heappush(lst, i)
+    
+    for i in lst:
+        answer += i**2
+        
+    return answer
--- Week19/common/p_12938.py
+++ Week19/common/p_12938.py
@@ -1,2 +1,43 @@
 # 최고의 집합
 # https://school.programmers.co.kr/learn/courses/30/lessons/12938
+
+
+def solution(n, s):
+    
+    if n > s:
+        return [-1]
+    
+    div = s // n
+    remain = s % n
+    
+    answer = [div] * n
+    
+    for i in range(remain):
+        answer[i] = answer[i] + 1
+    
+    answer.sort()
+    return answer
+
+
+
+# 시간초과
+# from itertools import combinations_with_replacement
+
+# def solution(n, s):
+#     combinations = combinations_with_replacement(range(1, s + 1), n)
+#     lst = [comb for comb in combinations if sum(comb) == s]
+#     answer = []
+    
+#     if not lst:
+#         return [-1]
+    
+#     for i in lst:
+#         arr = 1
+#         for j in i:
+#             arr *= j
+#         answer.append([arr,list(i)])
+    
+#     max_answer = max(answer, key=lambda x: x[0])
+#     result = max_answer[1]
+    
+#     return result
