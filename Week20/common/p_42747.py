5e397e4e964d7c756d3ea0dc998ad5e6a97beda1
Kurman11 <skedyd45@naver.com>
2023-11-25 22:03:51 +0900

    [Week20]23.11.25

--- Week20/common/p_42747.py
+++ Week20/common/p_42747.py
@@ -1,2 +1,15 @@
 # H-index
 # https://school.programmers.co.kr/learn/courses/30/lessons/42747
+
+def solution(citations):
+    citations.sort()
+    index = []
+    
+    for i in range(len(citations)):
+        if citations[i] >= len(citations)-i:
+            return len(citations) - i
+    return 0
+
+
+#    citations	  return
+# [3, 0, 6, 1, 5]	3
